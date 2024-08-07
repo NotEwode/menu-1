840cd00e2bf2135570a9ce8c8d7d9124e0e21b53
Ewode <147662427+NotEwode@users.noreply.github.com>
2024-08-07 23:44:14 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,30 @@
 function toggleItems(categoryId) {
-  var items = document.getElementById(categoryId);
-  items.classList.toggle('show');
+    var items = document.getElementById(categoryId);
+    if (items.classList.contains('show')) {
+        items.classList.remove('show');
+    } else {
+        items.classList.add('show');
+    }
 
-  var categoryTitle = document.querySelector(`h2.category-title[onclick="toggleItems('${categoryId}')"]`);
-  var icon = categoryTitle.querySelector('i');
+    var categoryTitle = document.querySelector(h2.category-title[onclick="toggleItems('${categoryId}')"]);
+    var icon = categoryTitle.querySelector('i');
 
-  icon.style.transform = icon.style.transform === 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
+    if (icon.style.transform === 'rotate(180deg)') {
+        icon.style.transform = 'rotate(0deg)';
+    } else {
+        icon.style.transform = 'rotate(180deg)';
+    }
 }
-
 let rotated = false;
 let sidebar = document.getElementById('sidebar');
-let rotateButton = document.getElementById('rotate');
 
-function toggleSidebar() {
+document.getElementById('rotate').addEventListener('click', function() {
   if (rotated) {
-      rotateButton.style.transform = 'rotate(0deg)';
-      sidebar.style.transform = 'translateX(100vw)';
+    this.style.transform = 'rotate(0deg)';
+    sidebar.style.transform = 'translateX(100vw)';
   } else {
-      rotateButton.style.transform = 'rotate(90deg)';
-      sidebar.style.transform = 'translateX(0)';
+    this.style.transform = 'rotate(90deg)';
+    sidebar.style.transform = 'translateX(0)';
   }
   rotated = !rotated;
-}
-
-rotateButton.addEventListener('click', toggleSidebar);
-rotateButton.addEventListener('touchstart', toggleSidebar);
+});
